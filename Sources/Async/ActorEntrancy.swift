9732cc322ff2b059c65cf9f8d14b949813d89601
Ivan Oparin <oparin_ig@hotmail.com>
2024-07-30 20:56:58 +0400

    Add synchronization primitives

---
+++ Sources/Async/ActorEntrancy.swift
@@ -0,0 +1,14 @@
+public final class ActorEntrancy {
+    private let semaphore = AsyncSemaphore(value: 1)
+
+    public init() {
+    }
+
+    public func enter() async {
+        await semaphore.wait()
+    }
+	
+    public func leave() {
+        semaphore.signal()
+    }
+}
